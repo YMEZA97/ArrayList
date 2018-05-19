563e660722c974fd64f25fc49f6ea67f9748b37f
YMEZA97 <39015245+YMEZA97@users.noreply.github.com>
2018-05-19 17:10:37 -0600

    Add files via upload

---
+++ src/ejercicios4/Exercises.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+
+/**
+ *
+ * @author turupawn
+ */
+public class Exercises {
+
+    static int getSum(ArrayList<Integer> list) {
+
+        int suma = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+
+            suma += list.get(i);
+        }
+
+        return suma;
+    }
+
+    static boolean exists(ArrayList<String> list, String value) {
+
+        boolean r = false;
+        if (list.contains(value)) {
+            r = true;
+        }
+
+        return r;
+    }
+
+    static ArrayList<Integer> sort(ArrayList<Integer> list) {
+                
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        Collections.sort(list);
+        
+        
+        return list;
+    }
+}
